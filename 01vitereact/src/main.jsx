9d07983aaf4e764749468396190af49cf42b535d
Koustavjr <Koustavjr11@gmail.com>
2024-05-02 00:35:10 +0530

    custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,34 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+/*const TestElement=(
+  <a href="https://google.com" target='_blank'>visit google</a>
+)
+*/
+
+/*function myApp()
+{
+  return (
+    <a href="https://google.com" target='_blank'>visit google</a>
+  )
+}*/
+
+// variable is rendered at the end and it is evaluated expression
+
+const chai='chai aur code'
+
+// createElement method operations are done by babel
+const customElement = React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'visit google',
+  chai
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  //TestElement               // another way to render
+  // <App/>
+  //myApp()                // it can run functions
+  customElement  
 )
---
+++ customReact/customreact.js
@@ -0,0 +1,36 @@
+// returning an 'a' tag,how react sees the element after compilation. It tries to create a tree like structure
+const reactElement={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children: 'click me to visit google',
+}
+
+const mainContainer=document.querySelector('#root');
+
+// first parameter what to ineject, 2nd one where to inject
+
+function customRender(reactElement,mainContainer)
+{
+/*    const domElement=document.createElement(reactElement.type);
+    domElement.innerHTML=reactElement.children;
+    domElement.setAttribute('href',reactElement.href);
+    domElement.setAttribute('target',reactElement.target);
+    
+    mainContainer.appendChild(domElement);*/
+
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML=reactElement.children;
+    for (const prop in reactElement.props) {
+       if(prop==='children') continue;
+
+       domElement.setAttribute(prop,reactElement.props[prop]);
+       domElement.setAttribute(prop,reactElement.props[prop]);
+    }
+    mainContainer.appendChild(domElement);
+}
+
+
+customRender(reactElement,mainContainer)
