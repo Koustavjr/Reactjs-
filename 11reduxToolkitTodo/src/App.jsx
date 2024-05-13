5ec334b0261c9d29ebc3847f79ef7a00f8e110e3
Koustavjr <Koustavjr11@gmail.com>
2024-05-13 22:58:55 +0530

    todo with redux toolkit

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import AddTodo from './componets/AddTodo'
+import Todos from './componets/Todos'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>TODO</h1>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
